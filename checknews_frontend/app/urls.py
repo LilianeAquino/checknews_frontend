f1880654bdf93685a3f5f491a6799e059cddf30c
Liliane L. de Aquino <liliane.lourenca@camss.group>
2023-05-11 18:33:38 -0300

    feat: configura as rotas apps

--- checknews_frontend/app/urls.py
+++ checknews_frontend/app/urls.py
@@ -6,4 +6,9 @@ app_name = 'app'
 urlpatterns = [
     path('', views.index, name='index'),
     path('login/', views.login, name='login'),
+    path('listagem/', views.login, name='listagem'),
+    path('checagem/', views.login, name='checagem'),
+    path('perfil/', views.login, name='perfil'),
+    path('sobre/', views.login, name='sobre'),
+    path('admin/', views.login, name='admin'),
 ]
--- checknews_frontend/app/views.py
+++ checknews_frontend/app/views.py
@@ -7,3 +7,23 @@ def index(request):
 
 def login(request):
     return render(request, 'app/login/login.html')
+
+
+def listagem(request):
+    return render(request, 'app/listagem/listagem.html')
+
+
+def checagem(request):
+    return render(request, 'app/checagem/checagem.html')
+
+
+def perfil(request):
+    return render(request, 'app/perfil/perfil.html')
+
+
+def sobre(request):
+    return render(request, 'app/sobre/sobre.html')
+
+
+def admin(request):
+    return render(request, 'app/admin/admin.html')
