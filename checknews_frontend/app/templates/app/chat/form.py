11f0aa006c0643a0f8ede4196ef61013af3df355
lilianelou <lilianelou@hotmail.com>
2023-06-16 11:41:15 -0300

    feat: implementa funcionalidade chat

--- checknews_frontend/app/templates/app/chat/form.py
+++ checknews_frontend/app/templates/app/chat/form.py
@@ -4,4 +4,4 @@ from app.models import Chat
 class ChatForm(forms.ModelForm):
     class Meta:
         model = Chat
-        fields = ('Remetente', 'Telefone', 'Assunto', 'Mensagem')
+        fields = ('sender', 'phone', 'subject', 'message')
