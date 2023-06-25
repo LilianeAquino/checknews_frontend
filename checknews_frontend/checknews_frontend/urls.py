7f15cdc09e1e3dedc564e010425134f28af7fcc9
lilianelou <lilianelou@hotmail.com>
2023-06-25 11:56:43 -0300

    refactor: ajusta responsavidade

--- checknews_frontend/checknews_frontend/urls.py
+++ checknews_frontend/checknews_frontend/urls.py
@@ -2,7 +2,6 @@ from django.urls.conf import include
 from django.contrib import admin
 from django.urls import path
 
-
 urlpatterns = [
     path('', include('app.urls', namespace='app')),
     path('admin/', admin.site.urls),
